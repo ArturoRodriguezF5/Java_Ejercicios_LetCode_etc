20465a37ac500e75b6e4125c3b0f5797f13271c5
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-10-05 19:24:07 -0600

    Commit para subir cambios realizados en ejercicios.

---
+++ Ejercicios/src/org/arthycode/java/letcode/ReverseCad.java
@@ -0,0 +1,32 @@
+package org.arthycode.java.letcode;
+
+public class ReverseCad {
+
+    public static void main(String[] args) {
+        String cadOri = "hola como cad";
+        StringBuilder cadRe = new StringBuilder();
+        char c;
+        for (int i = 0; i < cadOri.length(); i++) {
+            c = cadOri.charAt(i);
+            cadRe.insert(0, c);
+        }
+        System.out.println(cadRe);
+        /*char[] cadArr = cadOri.toCharArray();
+        char[] cadArrRe = new char[cadArr.length];
+        for (int i = 0; i < cadArr.length; i++) {
+            for (int j = cadArrRe.length; j >= 0; j++) {
+                cadArrRe[j] = cadArr[i];
+            }
+
+        }
+        System.out.println(cadArrRe);
+        System.out.println(cadArr);*/
+        String[] arrPal = {"Hola", "soy", "Arthy"};
+        String newPal = "";
+        for (int i = arrPal.length -1; i >= 0; i--) {
+            System.out.println(arrPal[i]);
+            newPal = newPal + arrPal[i] + " ";
+        }
+        System.out.println(newPal);
+    }
+}
